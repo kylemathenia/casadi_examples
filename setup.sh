8db329086b9cefe545e3b2551017711a5e5f33e3
Kyle Mathenia <kylemathenia@gmail.com>
2025-02-14 21:45:28 -0800

    let there be life

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo ✅ Setup complete! Run "source venv/bin/activate to activate the virtual environment.
