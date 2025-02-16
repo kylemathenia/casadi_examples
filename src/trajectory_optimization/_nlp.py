40ae30a3573f41708421db16904e607fc2a1337d
Kyle Mathenia <kylemathenia@gmail.com>
2025-02-15 17:13:40 -0800

    made a NlpPhase.mesh attribute

--- src/trajectory_optimization/_nlp.py
+++ src/trajectory_optimization/_nlp.py
@@ -1,15 +1,17 @@
 """Nonlinear programming structures that are independent of the transcription method."""
 
+from __future__ import annotations
 import casadi as ca
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ocp import OcpPhase
+    from mesh import OcpMesh
 
 
 class NlpPhase:
     def __init__(self, ocp: OcpPhase):
         self.ocp: OcpPhase = ocp
         self.opti: ca.Opti = ca.Opti()
-        self.mesh: Mesh = ocp.options.mesh.mesh
+        self.mesh: OcpMesh = ocp.options.mesh.mesh
